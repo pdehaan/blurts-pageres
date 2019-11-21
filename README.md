cbea0ea8890d6b4aae0720a28af515a51ee792f3
Peter deHaan <peter@deseloper.com>
2019-11-21 13:45:35 -0800

    FIRST BLOOD

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # blurts-pageres
+
 Firefox Monitor screenshots using pageres.
+
+This script will grab the latest breach list from the production https://monitor.firefox.com site, and then loop over each breach and take a cropped screenshot of the breach details header with the breach logo, name, type.
+
+## USAGE
+
+```sh
+git clone https://github.com/pdehaan/blurts-pageres.git blurts-pageres
+cd blurts-pageres
+npm install
+node index
+# wait and wait and wait
+```
+
+After screenshotting all the breach details pages, the cropped screenshots should be in a "./shots/" folder.
---
+++ index.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const Pageres = require("pageres");
+
+process.setMaxListeners(0);
+
+main()
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+async function main(server = "https://monitor.firefox.com", dest = "./shots", selector = "main#breach-detail div.flx-cntr") {
+  const breaches = await getBreaches(server);
+  for (const breach of breaches) {
+    const href = new URL(`/breach-details/${breach.Name}`, server).href;
+    await new Pageres({ delay: 0.1 })
+      .src(
+        href,
+        ["600x800"],
+        {
+          crop: true,
+          selector,
+          filename: `${breach.Name}-<%= size %>`
+        }
+      )
+      .dest(dest)
+      .run();
+  }
+}
+
+function getBreaches(server, count = Infinity) {
+  const href = new URL("/hibp/breaches", server).href;
+  console.log(`fetching ${href}`);
+  return axios
+    .get(href)
+    .then(res => res.data.slice(0, count));
+}
